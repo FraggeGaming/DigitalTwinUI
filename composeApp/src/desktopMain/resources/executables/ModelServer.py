97a2582d022bae15d8178c4dfaa34fc55edcf171
fardis <Fardisnazem@gmail.com>
2025-05-01 01:34:31 +0200

    Added the updated server code

--- composeApp/src/desktopMain/resources/executables/ModelServer.py
+++ composeApp/src/desktopMain/resources/executables/ModelServer.py
@@ -10,8 +10,11 @@ from subprocess import Popen
 import threading
 import platform
 import signal
+
+
 app = Flask(__name__)
 
+#Dataclass for providing the models
 @dataclass
 class AIModel:
     id: int
@@ -57,30 +60,33 @@ PROGRESS_DIR = os.path.join(BASE_DIR, "progress_files")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(PROGRESS_DIR, exist_ok=True)
+
+
+#the running processes and its lock is for keeping track of the process so it can be discarded if the user wants to cancel
 running_processes = {}  #save the running process that runs the model
 lock = threading.Lock() #lock for saving the process
 
-def start_subprocess(command, env):
-    if platform.system() == "Windows":
-        process = subprocess.Popen(
-            command,
-            shell=True,
-            env=env,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP  # Windows
-        )
-    else:
-        process = subprocess.Popen(
-            command,
-            shell=True,
-            env=env,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            preexec_fn=os.setsid  # Linux/macOS
-        )
-    return process
+progress_state = {}  #Dict for accessing the latest progress update for each running job_id
+progress_lock = threading.Lock()
 
+@app.route("/modalities", methods=["GET"])
+def get_modalities():
+    try:
+        modalities = sorted(set(model.inputModality for model in AVAILABLE_MODELS))
+        return jsonify(modalities)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/regions", methods=["GET"])
+def get_regions():
+    try:
+        regions = sorted(set(model.region for model in AVAILABLE_MODELS))
+        return jsonify(regions)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+#Function to kill the subprocess
 def kill_subprocess(process):
     if platform.system() == "Windows":
         print("[Server] Sending CTRL_BREAK_EVENT to Windows process")
@@ -90,7 +96,7 @@ def kill_subprocess(process):
         os.killpg(os.getpgid(process.pid), signal.SIGKILL)
     process.wait()
 
-
+#Get call to fetch the compatible models for the UI based on region, modality etc. Returns the compatible models back to the UI
 @app.route("/getmodels", methods=["POST"])
 def get_models():
     print("Received POST request to /getmodels")
@@ -129,27 +135,38 @@ def get_models():
         return jsonify({"error": str(e)}), 500
 
 
-@app.route("/progress/<job_id>", methods=["GET"])
+#function for reading the process stdout to get progress update from the inference. Saving the progress inside progress_state dict for the said job_id
+def read_progress(job_id, process):
+    marker = "::PROGRESS::"
+    for raw_line in process.stdout:
+        line = raw_line.strip()
+
+        # Skip blank lines
+        if not line:
+            continue
+
+        if marker in line:
+            try:
+                _, payload = line.split(marker, 1)
+                data = json.loads(payload.strip())
+                with progress_lock:
+                    progress_state[job_id] = data
+            except Exception as e:
+                print(f"[{job_id}] Failed to parse progress: {e}")
+
+        print(f"[{job_id} LOG]", line)
+
+
+#Fetches the progress json gathered from the stdout of the inference
+@app.route("/progress/<job_id>")
 def get_progress(job_id):
-    path = os.path.join(PROGRESS_DIR, f"progress_{job_id}.json")
-    if not os.path.exists(path):
-        #If the file does not exist, just return a "waiting" progress status
-        return jsonify({
-            "step": 0,
-            "total": 1,  # prevent divide-by-zero
-            "percent": 0.0,
-            "job_id": job_id,
-            "finished": False
-        })
+    with progress_lock:
+        progress = progress_state.get(job_id)
+    if not progress:
+        return '', 204
+    return jsonify(progress)
 
-    try:
-        with open(path, 'r') as f:
-            progress = json.load(f)
-        return jsonify(progress)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
+#Fetches the nifti based on the job_id. The nifti needs to be saved as {job_id}.nii.gz in order for the function to find it
 @app.route("/download/<job_id>", methods=["GET"])
 def download_output(job_id):
 
@@ -160,21 +177,14 @@ def download_output(job_id):
     denorm_img = nib.Nifti1Image(denormalized_data, img.affine, img.header)
     nib.save(denorm_img, nifti_output)
 
-    remove_progress_file(job_id)
     remove_uploaded_nifti(job_id)
     if not os.path.exists(nifti_output):
         return "Output file not ready yet.", 404
 
     return send_file(nifti_output, mimetype="application/octet-stream", as_attachment=True)
 
-def remove_progress_file(job_id):
-    path = os.path.join(PROGRESS_DIR, f"progress_{job_id}.json")
-    if os.path.exists(path):
-        os.remove(path)
-        print(f"Deleted progress file: {path}")
-    else:
-        print(f"Progress file not found: {path}")
 
+#function to remove the uploaded nifti file. Gets called after the user has downloaded the generated nifti
 def remove_uploaded_nifti(job_id):
     path = os.path.join(UPLOAD_DIR, f"{job_id}.nii.gz")
     if os.path.exists(path):
@@ -183,6 +193,9 @@ def remove_uploaded_nifti(job_id):
     else:
         print(f"Progress file not found: {path}")
 
+
+
+#Cancels the job if its running.
 @app.route("/cancel/<job_id>", methods=["POST"])
 def cancel_job(job_id):
     print(f"[Server] Trying to cancel job: '{job_id}'")
@@ -192,115 +205,80 @@ def cancel_job(job_id):
         if process:
             kill_subprocess(process)
             del running_processes[job_id]
-            remove_progress_file(job_id)
             remove_uploaded_nifti(job_id)
 
             return jsonify({"status": "Cancelled"}), 200
         else:
             return jsonify({"error": "No such job running"}), 404
 
+
+#Starts a subprocess and runs the model inference for the said model and the user added nifti
 @app.route("/process", methods=["POST"])
 def process_nifti():
     uploaded_file = request.files.get("file")
     metadata_json = request.form.get("metadata")
 
-
     if not uploaded_file or not metadata_json:
         return "Missing file or metadata", 400
-
-
 
     metadata = json.loads(metadata_json)
     print("Received metadata:", metadata)
 
 
+    # Save uploaded NIfTI file
+    upload_path = os.path.join(UPLOAD_DIR, f"{metadata['title']}.nii.gz")
+    uploaded_file.save(upload_path)
+    print(f"Saved uploaded file to: {upload_path}")
+
+    job_id = metadata['title']
+    mod = metadata['modality']
+    region = metadata['region']
+    model = metadata['model']
+    model_id = model.get("id")
+
+    name = 'SORTED+GROUPED_district+WARMUP_1'
+    which_epoch = 'BEST_final_200'
+    test_district = region
+
+
+    command = [
+        "python", TEST_SCRIPT,
+        "--gpu_ids", "-1",
+        "--json_id", job_id,
+        "--dataroot", DATA_PATH,
+        "--test_district", test_district,
+        "--which_epoch", str(which_epoch),
+        "--name", name,
+        "--out_path", OUTPUT_DIR,
+        "--upload_dir", upload_path,
+        "--checkpoints_dir", CHECKPOINTS_DIR,
+    ]
+
+    print(f"Running model command:\n{command}")
     try:
-        # Save uploaded NIfTI file
-        upload_path = os.path.join(UPLOAD_DIR, f"{metadata['title']}.nii.gz")
-        uploaded_file.save(upload_path)
-        print(f"Saved uploaded file to: {upload_path}")
-
-        job_id = metadata['title']
-
-
-        print(f"Upload path to be written into CSV: {upload_path}")
-        print(f"Does file exist? {os.path.exists(upload_path)}")
-
-
-        # Create correct test.csv file in the same folder that `--dataroot` points to
-        test_csv_path = os.path.join(DATA_PATH, "test.csv")
-        with open(test_csv_path, mode="w") as f:
-            f.write(upload_path.replace("\\", "/") + "\n")
-
-        mod = metadata['modality']
-        region = metadata['region']
-        model = metadata['model']
-
-        model_id = model.get("id")
-
-        #run different models based on the id (specified at the top of the script)
-        #if model_id == 1:
-        #   runModel(upload_path)
-        #if model_id == 2:
-        #    runModel(upload_path)
-
-        name = 'SORTED+GROUPED_district+WARMUP_1'
-        which_epoch = 'BEST_final_200'
-        test_district = region
-
-        # Launch subprocess with env var
-        env = os.environ.copy()
-
-        env["PROGRESS_FILE"] = os.path.join(PROGRESS_DIR, f"progress_{job_id}.json")
-        command = (
-            f'python "{TEST_SCRIPT}" '
-            f'--gpu_ids -1 '
-            f'--json_id "{job_id}" '
-            f'--dataroot "{DATA_PATH}" '
-            f'--test_district "{test_district}" '
-            f'--which_epoch {which_epoch} '
-            f'--name "{name}" '
-            f'--out_path "{OUTPUT_DIR}" '
-            f'--upload_dir "{upload_path}" '
-            f'--checkpoints_dir "{CHECKPOINTS_DIR}"'
+        process = subprocess.Popen(
+            command,
+            shell=False,
+            stdout=subprocess.PIPE,    #capture stdout so we can read it from the parent process
+            stderr=subprocess.STDOUT,
+            text=True,
+            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP if platform.system() == "Windows" else 0,
+            preexec_fn=os.setsid if platform.system() != "Windows" else None,
         )
 
-        print(f"Running model command:\n{command}")
-        try:
-            process = start_subprocess(command, env)
-            with lock:
-                running_processes[job_id] = process
+        threading.Thread(
+            target=read_progress,
+            args=(job_id, process),
+            daemon=True
+        ).start()
 
-        except Exception as e:
-            print(f"Error running command: {e}")
-            return f"Error starting model: {e}", 500
-
-
-        print("We Got HERE atleast------------")
-
-
+        running_processes[job_id] = process
         return jsonify({"status": "Running model"}), 200
 
-
-        #return send_file(nifti_output, mimetype="application/octet-stream", as_attachment=True)
-
-
     except Exception as e:
-        print("what happened here?")
-        return f"Error: {e}", 500
-
-
-
-
-import tempfile
-import threading
-import time
-
-
+        print(f"Error running command: {e}")
+        return f"Error starting model: {e}", 500
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
-
-
