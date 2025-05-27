3f515944ea010f5aeadbcd63cc0d0ebc68153541
fardis <Fardisnazem@gmail.com>
2025-05-27 21:35:15 +0200

    License for opensource

--- composeApp/src/desktopMain/kotlin/org/thesis/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/thesis/project/main.kt
@@ -5,6 +5,22 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 
+/*
+ * This file is part of [DeepTwin-X]
+ * Copyright (C) 2025 [Fardis Nazemroaya Sedeh]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
